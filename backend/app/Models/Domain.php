03b6458156023adee0c0b4e03990a65cb05ebb5b
Alex Lin <alexlin1822@hotmail.com>
2021-10-06 11:55:30 -0400

    Updated Domain Model

--- backend/app/Models/Domain.php
+++ backend/app/Models/Domain.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Domain extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the Domain model.
+     *
+     * @var string
+     */
+    protected $table = 'Domain';
+    public $timestamps = false;
+
+    public function domain() {
+        return $this->hasMany(Brand::class, 'device_id');  //TODO
+    }
+}
 }
