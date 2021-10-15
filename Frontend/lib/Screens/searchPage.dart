4a73690becb3a6bffa03518fabd56aa7af39221e
Yangmiao Wu <yangmiaowu825@gmail.com>
2021-10-14 23:31:04 -0400

    Add SearchBar and autosuggest

--- Frontend/lib/Screens/searchPage.dart
+++ Frontend/lib/Screens/searchPage.dart
@@ -1,26 +1,138 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:substring_highlight/substring_highlight.dart';
 
-class searchPage extends StatelessWidget {
-  List<Widget> pageChildren(double width) {
-    return <Widget>[
-    ];
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => new _HomeScreenState();
+}
+
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isLoading = false;
+
+  late List<String> autoCompleteData;
+
+  late TextEditingController controller;
+
+  Future fetchAutoCompleteData() async {
+    setState(() {
+      isLoading = true;
+    }
+    );
+
+    final String stringData = await rootBundle.loadString("assets/data.json");
+
+    final List<dynamic> json = jsonDecode(stringData);
+
+    final List<String> jsonStringData = json.cast<String>();
+
+    setState(() {
+      isLoading = false;
+      autoCompleteData = jsonStringData;
+    }
+    
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchAutoCompleteData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        if (constraints.maxWidth > 800) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: pageChildren(constraints.biggest.width / 2),
-          );
-        } else {
-          return Column(
-            children: pageChildren(constraints.biggest.width),
-          );
-        }
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Auto complete"),
+      ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+            padding: const EdgeInsets.symmetric(vertical: 200.0),
+              child: Column(
+                children: <Widget>[
+                  Autocomplete(
+                    optionsBuilder: (TextEditingValue textEditingValue) {
+                      if (textEditingValue.text.isEmpty) {
+                        return const Iterable<String>.empty();
+                      } else {
+                        return autoCompleteData.where((word) => word
+                            .toLowerCase()
+                            .contains(textEditingValue.text.toLowerCase()));
+                      }
+                    },
+                    optionsViewBuilder:
+                        (context, Function(String) onSelected, options) {
+                      return Material(
+                        elevation: 4,
+                        child: ListView.separated(
+                          padding: EdgeInsets.zero,
+                          itemBuilder: (context, index) {
+                            final option = options.elementAt(index);
+
+                            return ListTile(
+                              // title: Text(option.toString()),
+                              title: SubstringHighlight(
+                                text: option.toString(),
+                                term: controller.text,
+                                textStyleHighlight: TextStyle(fontWeight: FontWeight.w700),
+                              ),
+                              subtitle: Text("This is subtitle"),
+                              onTap: () {
+                                onSelected(option.toString());
+                              },
+                            );
+                          },
+                          separatorBuilder: (context, index) => Divider(),
+                          itemCount: options.length,
+                        ),
+                      );
+                    },
+                    onSelected: (selectedString) {
+                      print(selectedString);
+                    },
+                    fieldViewBuilder:
+                        (context, controller, focusNode, onEditingComplete) {
+                      this.controller = controller;
+
+                      return TextField(
+                        controller: controller,
+                        focusNode: focusNode,
+                        onEditingComplete: onEditingComplete,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.grey[300]!),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.grey[300]!),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.grey[300]!),
+                          ),
+                          hintText: "Search Something",
+                          prefixIcon: Icon(Icons.search),
+                        ),
+                      );
+                      
+                    },
+                    
+                  )
+                ],
+                
+              ),
+            ),
+            
     );
   }
 }
+  
+
--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:myapp/Screens/homePage.dart';
 import 'package:myapp/Components/Navbar.dart';
 import 'package:myapp/Screens/resultPage.dart';
 import 'package:myapp/Screens/searchPage.dart';
+
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -85,29 +87,8 @@ class mysearchpage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        constraints: BoxConstraints.expand(),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [
-                Color.fromRGBO(215, 191, 255, 1.0),
-                Color.fromRGBO(0, 191, 255, 1.0)
-              ]),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Navbar(),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 40.0),
-                child: searchPage()
+                child: HomeScreen()
               )
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
