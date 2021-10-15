14e86d286cf04e9e8823b47fd7dc9a72515c0182
Yangmiao Wu <yangmiaowu825@gmail.com>
2021-10-15 19:25:50 -0400

    Add brand text view

--- Frontend/lib/Screens/searchPage.dart
+++ Frontend/lib/Screens/searchPage.dart
@@ -199,6 +199,81 @@ class _HomeScreenState extends State<HomeScreen> {
                       
                     },
                     
+                  ),
+
+                   Padding(
+                  padding: EdgeInsets.all(8.0),
+                      child: Text(' '),
+                    ),
+
+                  Autocomplete(
+                    optionsBuilder: (TextEditingValue textEditingValue) {
+                      if (textEditingValue.text.isEmpty) {
+                        return const Iterable<String>.empty();
+                      } else {
+                        return autoCompleteData.where((word) => word
+                            .toLowerCase()
+                            .contains(textEditingValue.text.toLowerCase()));
+                      }
+                    },
+                    optionsViewBuilder:
+                        (context, Function(String) onSelected, options) {
+                      return Material(
+                        elevation: 4,
+                        child: ListView.separated(
+                          padding: EdgeInsets.zero,
+                          itemBuilder: (context, index) {
+                            final option = options.elementAt(index);
+
+                            return ListTile(
+                              // title: Text(option.toString()),
+                              title: SubstringHighlight(
+                                text: option.toString(),
+                                term: controller.text,
+                                textStyleHighlight: TextStyle(fontWeight: FontWeight.w700),
+                              ),
+                              subtitle: Text("This is subtitle"),
+                              onTap: () {
+                                onSelected(option.toString());
+                              },
+                            );
+                          },
+                          separatorBuilder: (context, index) => Divider(),
+                          itemCount: options.length,
+                        ),
+                      );
+                    },
+                    onSelected: (selectedString) {
+                      print(selectedString);
+                    },
+                    fieldViewBuilder:
+                        (context, controller, focusNode, onEditingComplete) {
+                      this.controller = controller;
+
+                      return TextField(
+                        controller: controller,
+                        focusNode: focusNode,
+                        onEditingComplete: onEditingComplete,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.grey[300]!),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.grey[300]!),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(color: Colors.grey[300]!),
+                          ),
+                          hintText: "Select the brand",
+                          prefixIcon: Icon(Icons.home),
+                        ),
+                      );
+                      
+                    },
+                    
                   )
 
                 ],
