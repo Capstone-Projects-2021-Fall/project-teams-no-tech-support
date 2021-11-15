b5555da7cd402155d6e90d4229d9228894088883
Alex Lin <alexlin1822@hotmail.com>
2021-11-14 22:10:32 -0500

    updated auto fill function

--- Frontend/lib/globals.dart
+++ Frontend/lib/globals.dart
@@ -17,46 +17,6 @@ class Album {
   }
 }
 
-class JJUser {
-  final String name;
-  final String email;
-
-  JJUser(this.name, this.email);
-
-  JJUser.fromJson(Map<String, dynamic> json)
-      : name = json['device'],
-        email = json['brand'];
-
-  Map<String, dynamic> toJson() => {
-        'device': name,
-        'brand': email,
-      };
-}
-
-//Class for return RevisedQuery json
-// class TQuestion {
-//   String sDevice;
-//   String sBrand;
-//   String sModel;
-//   String sQuery;
-
-//   TQuestion({
-//     required this.sDevice,
-//     required this.sBrand,
-//     required this.sModel,
-//     required this.sQuery,
-//   });
-
-//   factory TQuestion.fromJson(Map<String, dynamic> json) {
-//     return TQuestion(
-//       sDevice: json['device'],
-//       sBrand: json['brand'],
-//       sModel: json['model'],
-//       sQuery: json['revisedQuery'],
-//     );
-//   }
-// }
-
 class comm {
   //load json to list for device model and brand
   static List<Album> loadJson(String jsonString) {
@@ -64,20 +24,14 @@ class comm {
     return parsed.map<Album>((json) => Album.fromJson(json)).toList();
   }
 
-  //load json to list for revise query device model and brand
-  // static List<TQuestion> loadQuestionJson(String jsonString1) {
-  //   final parsed1 = json.decode(jsonString1).cast<Map<String, dynamic>>();
-  //   return parsed1.map<TQuestion>((json) => TQuestion.fromJson(json)).toList();
-  // }
-
   static String mydevice = '';
   static String mybrand = '';
   static String mymodel = '';
   //need delete
-  // static String what = '';
-  // static String when = '';
-  // static String why = '';
-  //////////
+  static String what = '';
+  static String when = '';
+  static String why = '';
+  ////////
   static String question = '';
   static String reviseQuestion = '';
 
